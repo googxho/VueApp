f4d9b36d5bb3d7bc71a22be12113189e5a56396f
googxh <gxh522@qq.com>
2021-11-09 21:16:39 +0800

    使用路由插件

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <template>
-  <div class="container">
-    <Search />
-    <UserMain />
+  <div>
+    <div class="row">
+      <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header"><h2>Vue 路由</h2></div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-2 col-xs-offset-2">
+        <div class="list-group">
+          <!--生成路由链接-->
+          <router-link to="/about" class="list-group-item">About</router-link>
+          <router-link to="/home" class="list-group-item">Home</router-link>
+        </div>
+      </div>
+      <div class="col-xs-6">
+        <div class="panel">
+          <div class="panel-body">
+            <!--显示当前组件-->
+            <keep-alive>
+              <router-view msg="abc"></router-view>
+            </keep-alive>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Search from './components/Search.vue'
-import Main from './components/Main.vue'
-
-export default {
-  name: 'App',
-  data () {
-    return {
-      // input: 'eee'
-    }
-  },
-  components: {
-    Search, UserMain: Main
-  },
-  mothods: {
-  }
-}
+export default {}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import router from './router/index.js'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  components: {App}, // 映射组件标签
+  template: '<App />', // 指定需要渲染到页面的模板
+  router  // 注册路由器
 })
